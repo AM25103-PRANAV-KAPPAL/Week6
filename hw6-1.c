6e94c6cc49f289aaf51b75668f804168d5390bc7
AM25103-PRANAV-KAPPAL <am25103@sic.shibaura-it.ac.jp>
2025-11-19 13:42:21 +0900

    Update hw6-1.c

--- hw6-1.c
+++ hw6-1.c
@@ -1,1 +1,27 @@
-ss
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n < 2) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int max;
+    printf("Enter max: ");
+    scanf("%d", &max);
+
+    for (int i = 2; i < max; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
